30fc8f81aad7fb7a1c2f6f25325c879af2413c18
Steven Humam <oppaidaisuki363@gmail.com>
2021-11-21 12:32:43 +0700

    add bcrypt

--- drivers/repository/users/userRepository.go
+++ drivers/repository/users/userRepository.go
@@ -3,10 +3,9 @@ package users
 import (
 	"context"
 	"github.com/stevenfrst/crowdfunding-api/drivers/repository"
+	"github.com/stevenfrst/crowdfunding-api/helper/encrypt"
 	"github.com/stevenfrst/crowdfunding-api/usecase/users"
 	"gorm.io/gorm"
-	"log"
-	"reflect"
 )
 
 type UserRepository struct {
@@ -22,23 +21,35 @@ func NewUserRepository(gormDb *gorm.DB) users.UserRepoInterface {
 func (r *UserRepository) CheckLogin(email,password string,ctx context.Context) (users.Domain, error) {
 	var user repoModels.User
 
-	err := r.db.Where("email = ?", email,"password = ?",password).First(&user).Error
-	log.Println(err)
+	err := r.db.Where("email = ?", email).First(&user).Error
 	if err != nil {
 		return users.Domain{},err
 	}
+
+	err = encrypt.CheckPassword(password,user.Password)
+	if err != nil {
+		return users.Domain{},err
+	}
+
 	return user.ToDomain(),nil
 }
 
 func (r *UserRepository) Register(user *users.Domain,ctx context.Context) (users.Domain,error) {
-	//log.Println("HIT")
-	result := r.db.Create(repoModels.FromDomainUser(user))
-	log.Println(reflect.TypeOf(user),result.RowsAffected)
-	if result.Error != nil {
-		return *user,result.Error
+
+	userIn := repoModels.FromDomainUser(user)
+	hashedPassword,err := encrypt.Hash(user.Password)
+	if err != nil {
+		return users.Domain{},err
 	}
-	//var out repoModels.User
-	return *user,nil
+
+	userIn.Password = hashedPassword
+	result := r.db.Create(userIn)
+	//log.Println(reflect.TypeOf(user),result.RowsAffected)
+	if result.Error != nil {
+		return userIn.ToDomain(),result.Error
+	}
+
+	return userIn.ToDomain(),nil
 }
 
 func (r UserRepository) GetAllUser() ([]users.Domain,error) {
